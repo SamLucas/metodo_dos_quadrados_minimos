46e28ef9bccf61ec1fee89382e9e4c9d9ee821f1
Samuel Lucas <samuellucas0603@gmail.com>
2021-09-09 21:48:34 -0300

    adicionando estrutura do projeto.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,110 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import QuadradosMinimos from './QuadradosMinimos';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
+import styled from 'styled-components';
+
+export const CLineChart = styled(LineChart)`
+  width: 50vw;
+  height: 100vh;
+`;
+
 function App() {
 
-  const [data, setData] = useState()
+  const [points, setPoints] = useState([])
+  const [dataGraph, setDataGraph] = useState([])
+  const [additionalInformation, setAdditionalInformation] = useState()
+
+  const [X, setX] = useState('')
+  const [Y, setY] = useState('')
 
   useEffect(() => {
+    const { straightPoints, ...rest } = QuadradosMinimos(points)
+    setDataGraph(straightPoints)
+    setAdditionalInformation(rest)
 
-    let x = [0.3, 2.7, 4.5, 5.9, 7.8];
-    let y = [1.8, 1.9, 3.1, 3.9, 3.3];
+  }, [points])
 
-    const { pontos } = QuadradosMinimos(x, y)
-    setData(pontos)
-  }, [])
+  const registerNewPoints = () => {
+    setPoints(s => [...s, { x: X, y: Y }])
+    document.getElementById("inputX").value = ""
+    document.getElementById("inputY").value = ""
+  }
+
+  return (
+    <div>
+
+      <input type="number" id="inputX" onChange={e => setX(e.target.value)} />
+      <input type="number" id="inputY" onChange={e => setY(e.target.value)} />
+
+      <button onClick={registerNewPoints}>Adicionar</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>I</th>
+            <th>X</th>
+            <th>Y</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataGraph.map((e, idx) => (
+            <tr>
+              <td>{idx}</td>
+              <td>{e.x}</td>
+              <td>{e.y}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <table>
+        <thead>
+          <tr>
+            <th>i</th>
+            <th>x</th>
+            <th>y</th>
+            <th>µ</th>
+            <th>d</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataGraph.map((e, idx) => (
+            <tr>
+              <td>{idx}</td>
+              <td>{e.x}</td>
+              <td>{e.y}</td>
+              <td>{e.newY}</td>
+              <td>{e.newX}</td>
+            </tr>
+          ))}
+          <tr>
+            <td>Total</td>
+            <td>{dataGraph.reduce((s, { x }) => s += x, 0)}</td>
+            <td>{dataGraph.reduce((s, { y }) => s += y, 0)}</td>
+            <td>{dataGraph.reduce((s, { newY }) => s += newY, 0)}</td>
+            <td>{dataGraph.reduce((s, { newX }) => s += newX, 0)}</td>
+          </tr>
+        </tbody>
+      </table>
 
 
-  return <div>
-    <LineChart width={800} height={300} data={data}>
-      <Line type="monotone" dataKey="y" stroke="#8884d8" />
-      <Line type="monotone" dataKey="mi" stroke="red" />
-      <XAxis dataKey="x" />
-      <XAxis dataKey="dis" />
-      <CartesianGrid stroke="#ccc" />
-      <YAxis />
-      <Tooltip />
-    </LineChart>
-  </div>;
+      <h3>{additionalInformation?.B1}</h3>
+      <h3>{additionalInformation?.B0}</h3>
+      <h3>{additionalInformation?.SomaQuadradoDistacia}</h3>
+
+      <CLineChart width={600} height={600} data={dataGraph}>
+        <Line type="monotone" dataKey="y" stroke="#8884d8" />
+        <Line type="monotone" dataKey="newY" stroke="red" />
+        <XAxis dataKey="x" />
+        <XAxis dataKey="newX" />
+        <CartesianGrid stroke="#ccc" />
+        <YAxis />
+        <Tooltip />
+      </CLineChart>
+    </div>
+  );
 }
 
 export default App;
--- src/QuadradosMinimos.js
+++ src/QuadradosMinimos.js
@@ -1,4 +1,10 @@
-export default QuadradosMinimos = (x, y) => {
+const QuadradosMinimos = (points) => {
+
+  // let x = [0.3, 2.7, 4.5, 5.9, 7.8];
+  // let y = [1.8, 1.9, 3.1, 3.9, 3.3];
+
+  let x = points.map(e => parseFloat(e.x))
+  let y = points.map(e => parseFloat(e.y))
 
   let n = x.length
 
@@ -14,9 +20,9 @@ export default QuadradosMinimos = (x, y) => {
   let B1 = ((somaX * somaY) - n * somaXMultiplicadoY) / (QuadradoDaSomaX - (n * somaQuadradosX));
   let B0 = (somaY - (B1 * somaX)) / n;
 
-  let D = 0.0;
+  let SomaQuadradoDistacia = 0.0;
 
-  let SomaQuadradoDistacia = []
+  let pontos = []
 
   x.forEach((a, idx) => {
 
@@ -25,15 +31,19 @@ export default QuadradosMinimos = (x, y) => {
     data.x = a
     data.y = y[idx]
 
-    data.newY = (B0 + (B1 * a)).toFixed(2);
-    data.newX = (y[idx] - data.mi).toFixed(2);
+    data.newY = (B0 + (B1 * a)).toFixed(4);
+    data.newX = (y[idx] - data.newY).toFixed(4);
 
-    SomaQuadradoDistacia += data.dis * data.dis;
+    SomaQuadradoDistacia += data.newX * data.newX;
     pontos.push(data)
   })
 
   return {
-    pontos,
-    SomaQuadradoDistacia
+    straightPoints: pontos,
+    SomaQuadradoDistacia: SomaQuadradoDistacia.toFixed(4),
+    B0: B0.toFixed(4),
+    B1: B1.toFixed(4),
   }
 }
+
+export default QuadradosMinimos;
